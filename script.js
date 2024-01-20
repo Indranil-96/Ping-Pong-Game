3ce46c2ba8cbf2d70835f93271f0e8a300a33426
Indranil-96 <141549310+Indranil-96@users.noreply.github.com>
2024-01-20 23:01:57 +0000

    Collison Logic Built

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ let gamebox=document.getElementById("gamebox");
 
 let wpressed=false;
 let spressed=false;
-let opressed=false;
-let lpressed=false;
+
+let user1=document.getElementById("user1");
+let user2=document.getElementById("user2");
 
 
 let p1val=50;
@@ -59,38 +60,47 @@ function movebar(){
         p1val+=5;
         paddle1.style.top=p1val+"%";
     }
-
-    else if(opressed && p2val>15){
-        p2val-=5;
-        paddle2.style.top=p2val+"%";
-    }
-
-    else if(lpressed && p2val<85){
-        p2val+=5;
-        paddle2.style.top=p2val+"%";
-    }
-    // requestAnimationFrame(movebar);
 }
 
 //Accourding to pythagoras theorem v^2=vx^2 + vy^2;
 
-let vx=-2;
-let vy=-15;
+let vx=-4;
+let vy=-4;
 let v= Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2));
 
 
 const reset=()=>{
     ball.style.top="50%";
     ball.style.left="50%";
-    vx=-2;
-    vy=-15;
+    vx=-4;
+    vy=-4;
     let v= Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2));
 }
+const checkCollition=(activePaddle)=>{
+    let balltop=ball.offsetTop;
+    let ballleft=ball.offsetLeft;
+    let ballbottom=ball.offsetTop+ball.offsetHeight;
+    let ballright=ball.offsetLeft+ball.offsetWidth;
 
+    let paddletop=activePaddle.offsetTop;
+    let paddleleft=activePaddle.offsetLeft;
+    let paddlebottom=activePaddle.offsetTop+activePaddle.offsetHeight;
+    let paddleright=activePaddle.offsetLeft+activePaddle.offsetWidth;
+
+    // console.log(balltop,ballbottom,ballleft,ballright);
+    // console.log(paddletop,paddlebottom,paddleleft,paddleright);
+
+    if(ballbottom>paddletop && balltop<paddlebottom && ballright > paddleleft && ballleft<paddleright){
+        console.log(" Collison Detected"); // Something Wrong here.
+        return true;
+    }else{
+        return false;
+    }
+}
 function moveball(){
     if(ball.offsetLeft<0){
-        // vx=-vx;
-        reset();
+        vx=-vx;
+        // reset();
     }
 
     if(ball.offsetLeft>gamebox.offsetWidth-(ball.offsetWidth/2)){
@@ -107,6 +117,21 @@ function moveball(){
 
     ball.style.left=ball.offsetLeft+vx+"px";
     ball.style.top=ball.offsetTop+vy+"px";
+
+
+    // Which one is the active panel ?
+
+    let position=ball.offsetLeft;
+    let boxwidth=gamebox.offsetWidth;
+
+    let paddle=position<boxwidth/2 ? user1 : user2;
+    // console.log(paddle);
+
+    let ballcenterY=ball.offsetTop+ball.offsetHeight/2;
+
+    let paddlecenterY=paddle.offsetTop+paddle.offsetHeight/2;
+    let angle=0;
+    checkCollition(paddle)
 
     requestAnimationFrame(moveball);
 }
